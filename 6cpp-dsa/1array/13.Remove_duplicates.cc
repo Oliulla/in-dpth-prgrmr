8c48712fa26976595fe45bf2d54254ca65d4684a
Oliulla <mohammad.oliullah25@gmail.com>
2025-05-21 12:08:30 +0600

    done: Problem 13

--- 6cpp-dsa/1array/13.Remove_duplicates.cc
+++ 6cpp-dsa/1array/13.Remove_duplicates.cc
@@ -4,16 +4,32 @@ using namespace std;
 
 class Solution {
     public:
-        vector<int> removeDuplicates(vector<int> nums) {
-        
+        vector<int> removeDuplicates(vector<int>& nums) {
+            int len = nums.size();
+            vector<int> unq;
+            for(int i = 0; i < len; i++) {
+                bool found = false;
+               for(int j = 0; j < unq.size(); j++) {
+                    if(unq[j] == nums[i]) {
+                        found = true;
+                        break;
+                    }
+                }
+                if(!found) unq.push_back(nums[i]);
+            }
+            return unq;
         }
 };
 
 int main() {
     Solution s;
-    vector<int> nums = {1, 2, 2, 3, 4, 5};
+    vector<int> nums = {1, 2, 2, 2, 2, 3, 4, 5, 1};
 
-    s.removeDuplicates(nums);
+    vector<int> unqNms = s.removeDuplicates(nums);
+    for (int i = 0; i < unqNms.size(); i++) {
+        cout << unqNms[i] << " ";
+    }
+    cout << endl;
 
     return 0;
 }
