746ab60a2bbc10dec42e5fc942544251ea71b323
Oliulla <mohammad.oliullah25@gmail.com>
2025-06-19 18:28:00 +0600

    done till 58

--- 6cpp-dsa/1array/58.Longest_increasing_subsequence.cc
+++ 6cpp-dsa/1array/58.Longest_increasing_subsequence.cc
@@ -7,22 +7,47 @@ using namespace std;
 class Solution {
 public:
     int longestIncreasingSubsequence(vector<int>& nums) {
-        int curr = nums[0];
-        int count = 1;
-        for (int i = 1; i < nums.size(); i++) {
-            if (curr < nums[i]) {
-                count++;
+        /*
+            int curr = nums[0];
+            int count = 1;
+            int currIdx = 0;
+            for (int i = 1; i < nums.size(); i++) {
+                if (curr < nums[i]) {
+                    count++;
+                    curr = nums[i];
+                    currIdx = i;
+                }
+                if (currIdx == 0) {
+                    curr = nums[i];
+                }
             }
-            curr = nums[i];
+            return count;
+        */
+
+        int n = nums.size();
+
+        vector<int> dp(n, 1);
+        int maxLen = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            maxLen = max(maxLen, dp[i]);
         }
-        return count;
+
+        // for (int num : dp) {
+        //     cout << num << endl;
+        // }
+        return maxLen;
     }
 };
 
 
 int main(void) {
     Solution s;
-    vector<int> nums = { 10,9,2,5,3,7,101,18 };
+    vector<int> nums = { 10, 9, 2, 5, 3, 7, 101, 18 };
     int res = s.longestIncreasingSubsequence(nums);
     cout << res << endl;
 
