1f2e2a95de1a78a56c9089386d8e87693c3dc1dc
Oliulla <mohammad.oliullah25@gmail.com>
2025-06-29 12:42:43 +0600

    solved rotateArrByKPosition with o(n)

--- 6cpp-dsa/1array/28.Rotate_array_by_K_positions.cc
+++ 6cpp-dsa/1array/28.Rotate_array_by_K_positions.cc
@@ -1,21 +1,44 @@
 #include<iostream>
 #include<vector>
+#include "print.h"
 using namespace std;
+
+// class Solution {
+// public:
+//     void rotateArrByKPosition(vector<int>& nums, int k) {
+//         // int j = k;
+//         // while (--j >= 0)
+//         // {
+//         //     for (int i = nums.size() - 1; i > 0; i--) {
+//         //         int temp = nums[i];
+//         //         nums[i] = nums[i - 1];
+//         //         nums[i - 1] = temp;
+//         //     }
+//         // }
+
+//     }
+// };
 
 class Solution {
 public:
-    vector<int> rotateArrByKPosition(vector<int>& nums, int k) {
-        int j = k;
-        while (--j >= 0)
-        {
-            for (int i = nums.size() - 1; i > 0; i--) {
-                int temp = nums[i];
-                nums[i] = nums[i - 1];
-                nums[i - 1] = temp;
+    void rotateArrByKPosition(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k % n;
+
+        vector<int> res;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (i < k) {
+                res.push_back(nums[n + i - k]);
+            }
+            else {
+                res.push_back(nums[i - k]);
             }
         }
 
-        return nums;
+        for (int i = 0; i < n; i++) {
+            nums[i] = res[i];
+        }
     }
 };
 
@@ -24,11 +47,7 @@ int main(void) {
     vector<int> nums = { 1, 2, 3, 4, 5 };
     int k = 2;
     s.rotateArrByKPosition(nums, k);
-
-    for (int num : nums) {
-        cout << num << " ";
-    }
-    cout << endl;
+    print(nums);
 
     // Output:
     // [4, 5, 1, 2, 3]
